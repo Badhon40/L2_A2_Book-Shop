0123e7a3de038aa852481a9e5b96b26626be73d0
Badhon40 <cse12005018brur@gmail.com>
2024-11-30 21:45:22 +0600

    connected with db

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,11 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { languageOptions: { globals: globals.node } },
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
@@ -17,8 +18,5 @@ export default [
       "no-console":"warn",
       "no-undef":"error"
     },
-    globals:{
-      "process":"readonly",
-    }
   },
 ];
--- src/modules/user/user.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { useController } from "./user.controller";
-
-
-const useRouter= Router()
-
-useRouter.post('/createUser',useController.createUser)
-
-export default useRouter;
